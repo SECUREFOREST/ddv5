d8dc58e4c8fa9a8aa44257802682abb6e3fc961a
John Doe <johndoe@example.com>
2025-07-09 17:38:52 +0300

    Remove MongoDB connection success log from db.js for cleaner code and reduced console output.

--- server/db.js
+++ server/db.js
@@ -9,8 +9,5 @@ mongoose.connect(DB_URI, {
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
 
 module.exports = db; 
