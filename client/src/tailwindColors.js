cad2de624395d963fdca5ec481cd0fdc508f8680
John Doe <johndoe@example.com>
2025-07-09 16:19:49 +0300

    Refactor various components to improve styling and layout using Tailwind CSS. Update background colors, text styles, and form elements for enhanced visual consistency and user experience across the application. Ensure cohesive design elements throughout.

---
+++ client/src/tailwindColors.js
@@ -0,0 +1,25 @@
+export const chartColors = {
+  yellow: '#ff0', // mark background
+  black: '#000', // text, borders
+  gray: '#888', // muted text
+  white: '#fff', // text, backgrounds
+  red: '#D60B20', // primary red
+  redDark: '#a50919',
+  orange: '#FF8800',
+  orangeDark: '#cc6d00',
+  green: '#77B300',
+  greenDark: '#558000',
+  purple: '#9933CC',
+  purpleDark: '#7a29a3',
+  background: '#060606', // main background
+  backgroundDark: '#181818',
+  backgroundDarker: '#090909',
+  backgroundLight: '#282828',
+  border: '#c0c0c0',
+  borderDark: '#282828',
+  info: '#e6e6e6',
+  warning: '#f9f2f4',
+  code: '#c7254e',
+  codeBg: '#f9f2f4',
+  // Add more as needed from 1.css
+}; 
